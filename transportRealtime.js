28d7c9f4945031c77d7bd70ddca5692c0580dc27
ArJau <arnaud.jaubertie@gmail.com>
2022-04-13 15:53:03 +0200

    transportRealtime

--- transportRealtime.js
+++ transportRealtime.js
@@ -8,18 +8,19 @@ async function init() {
 
   await modelRepo.initModels();
   let map = modelRepo.mapModel();
-  PersistentCircuitModel = map.get("circuits");
 
   let lstUrlRt = [];
   let criteria;
-  //criteria = { "resources.format": "gtfs-rt" };
-  //criteria = { "id": "55ffbe0888ee387348ccb97d" };
-  //criteria = { "id": "5b0d18ed88ee3836341f603d" };
-  criteria = { "id": "5dfcf2a4634f4110fc360457" };
-  
-  
+  criteria = { "resources.format": "gtfs-rt" };
 
-  PersistentCircuitModel.find(criteria, async function (err, lstCircuits) {
+  //criteria = { "id": "5f3b576ef388432cf6071cc6" };
+  //criteria = { "id": "55ffbe0888ee387348ccb97d" };//brest
+  //criteria = { "id": "5b0d18ed88ee3836341f603d" };
+  //criteria = { "id": "5dfcf2a4634f4110fc360457" };
+
+
+
+  map.get("circuits").find(criteria, async function (err, lstCircuits) {
     if (err) {
       console.log("err: " + err);
     }
@@ -39,8 +40,6 @@ async function init() {
 
           if (format == "gtfs-rt") {
             lstUrlRt.push({ id: id, url: url });
-          } else {
-            log("FORMAT: " + format + ", URL: " + url + ", id: " + id);
           }
 
         }
@@ -49,86 +48,120 @@ async function init() {
       else {
         log("Resource inexistante id: " + id, true);
       }
-      await loadRealTime(lstUrlRt);
     }
+    //lstUrlRt = lstUrlRt.slice(10);
+
+    for (let i in lstUrlRt) {
+      log("Circuit: " + (Number(i) + 1) + ", url:" + lstUrlRt[i].url);
+      await loadRealTime(lstUrlRt[i]);
+    }
+    console.log("..................fini.................")
   });
 }
 
 
-async function loadRealTime(lstUrlRt) {
+async function loadRealTime(urlRt) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let requestSettings = {
+        method: 'GET',
+        url: urlRt.url,
+        encoding: null
+      };
+      request(requestSettings, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          try {
+            var feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
+            let feedEntity = feed.entity;//.slice(0, 1);
+            let feedEntityAlert = [];
+            let feedEntityVehicle = [];
+            for (let numEntity in feedEntity) {
+              if (feedEntity[numEntity].vehicle) {
+                feedEntity[numEntity].idReseau = urlRt.id;//on rajoute l'id
+                feedEntityVehicle.push(feedEntity[numEntity]);
+              }
 
-  for (let i in lstUrlRt) {
-    let urlRt = lstUrlRt[i];
-
-    let requestSettings = {
-      method: 'GET',
-      url: urlRt.url,
-      //55ffbe0888ee387348ccb97d
-      encoding: null
-    };
-    request(requestSettings, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-
-        var feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
-        let feedEntity = feed.entity.slice(0, 1);
-
-        log(JSON.stringify(feedEntity), true);
-        for (let numEntity in feedEntity) {
-          if (feedEntity[numEntity].tripUpdate) {
-            stopTimeUpdate = feedEntity[numEntity].tripUpdate.stopTimeUpdate;
-            for (let numSeq in stopTimeUpdate) {
-              let stopTime = stopTimeUpdate[numSeq];
-              //console.log(stopTime);
-              /*let dateArrivee = new Date(stopTime.arrival.time.low * 1000);
-              let dateDepart = new Date(stopTime.departure.time.low * 1000);
-              log(stopTime.stopSequence + ". " + stopTime.stopId 
-              + ". " + dateArrivee.getUTCDate() + " " + dateArrivee.toLocaleTimeString() 
-              + "." + dateDepart.getUTCDate()  + " " + dateDepart.toLocaleTimeString(), true);*/
+              if (feedEntity[numEntity].alert) {
+                feedEntity[numEntity].idReseau = urlRt.id;//on rajoute l'id
+                feedEntityAlert.push(feedEntity[numEntity]);
+              }
             }
-            //console.log("-----------------------------------------------------------------");
-          }
-        }
+            if (feedEntityVehicle.length > 0) {
+              log("enregistrement feedEntityVehicle: url: " + requestSettings.url + ", nb: " + feedEntityVehicle.length);
+              insertDb("realTimesVehicles", urlRt.id, "realTimesVehicles", feedEntityVehicle);
+            }
+            if (feedEntityAlert.length > 0) {
+              log("enregistrement feedEntityAlert: url: " + requestSettings.url + ", nb: " + feedEntityAlert.length);
+              insertDb("realTimesAlert", urlRt.id, "realTimesAlert", feedEntityAlert);
+            }
 
-        //insertDb(lstUrlRt[i].id,"realTime", feedEntity);
-      }
-    });
-  }
+            resolve();
+          } catch (err) {
+            resolve();
+            log("ERROR : " + err);
+          }
+        } else {
+          resolve();
+        }
+      });
+    } catch (err) {
+      log("ERROR : " + err);
+      reject();
+    }
+  });
+
 }
-async function insertDb(id, model, tableauJson) {
-  return new Promise((resolve, reject) => {
+
+async function insertDb(fileName, id, model, tableauJson) {
+  return new Promise(async (resolve, reject) => {
     try {
       for (let i in tableauJson) {
-        tableauJson[i]["id"] = id;
+        tableauJson[i]["idReseau"] = id;
       }
 
       //console.log(tableauJson);
       let map = modelRepo.mapModel();
-      map.get(model).insertMany(tableauJson, (err, result) => {
+      map.get(model).insertMany(tableauJson, async (err, result) => {
         if (err) {
-          reject(log("CSV ERROR" + model + ",id: " + id, true));
+          log("CSV ERROR" + model + ",id: " + id)
+          reject();
         }
         if (result) {
-          resolve(log("CSV " + model + ",lignes:," + tableauJson.length + ", id: " + id + ", fileName=" + fileName, true));
+          resolve(log("CSV " + model + ",lignes: " + tableauJson.length + ", id: " + id + ", fileName=" + fileName));
         }
       });
     } catch (er) {
-      reject(log("CSV ERROR" + model + ",id: " + id + " fileName=" + fileName + ", er:" + er, true));
+      log("CSV ERROR" + model + ",id: " + id + " fileName=" + fileName + ", er:" + er)
+      reject();
     }
   });
 }
 
 function log(txt, isFichier) {
   const date = new Date();
-  const content = date.toISOString() + ", " + txt;
+  //const txt = date.toISOString() + ", " + txt;
 
   if (isFichier) {
-    var nomFichier = date.getUTCFullYear() + (pad(date.getUTCMonth()+ 1)) + date.getUTCDate() + ".log";
+    var nomFichier = date.getUTCFullYear() + (pad(date.getUTCMonth() + 1)) + date.getUTCDate() + ".log";
 
-    fs.appendFile('log/' + nomFichier, content + "\n", function (err) {
+    fs.appendFile('log/' + nomFichier, txt + "\n", function (err) {
       if (err) { throw err };
     });
   }
-  console.log(content);
+  console.log(txt);
+}
+
+function logError(txt, isFichier) {
+  const date = new Date();
+  //const txt = date.toISOString() + ", " + txt;
+
+  if (isFichier) {
+    var nomFichier = date.getUTCFullYear() + (pad(date.getUTCMonth() + 1)) + date.getUTCDate() + ".log.error";
+
+    fs.appendFile('log/' + nomFichier, txt + "\n", function (err) {
+      if (err) { throw err };
+    });
+  }
 }
 
 init();
